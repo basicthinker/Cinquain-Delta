b93cd3aeb40afdd0644caf09cc69761613141fc9
Jinglei Ren <jinglei.ren@gmail.com>
2012-02-05 16:13:19 +0800

    Add versioned_hashtable for delta-encoding.

---
+++ diff/lib/versioned_hashtable.cc
@@ -0,0 +1,9 @@
+//
+//  versioned_hashtable.cc
+//  Cinquain-Delta
+//
+//  Created by Jinglei Ren on 2/5/12.
+//  Copyright (c) 2012 Tsinghua University. All rights reserved.
+//
+
+#include <iostream>
---
+++ diff/lib/versioned_hashtable.h
@@ -0,0 +1,14 @@
+//
+//  versioned_hashtable.h
+//  Cinquain-Delta
+//
+//  Created by Jinglei Ren on 2/5/12.
+//  Copyright (c) 2012 Tsinghua University. All rights reserved.
+//
+
+#ifndef Cinquain_Delta_versioned_hashtable_h
+#define Cinquain_Delta_versioned_hashtable_h
+
+
+
+#endif
